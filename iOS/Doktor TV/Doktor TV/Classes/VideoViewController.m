06f2480cbd75df39441a5f04174816d25bdb2abf
Tobias Due Munk <tobiasdm@gmail.com>
2013-11-14 00:44:06 +0100

    iOS: Setup test interface. Stream video √. Download video √.

--- iOS/Doktor TV/Doktor TV/Classes/VideoViewController.m
+++ iOS/Doktor TV/Doktor TV/Classes/VideoViewController.m
@@ -9,6 +9,7 @@
 #import "VideoViewController.h"
 
 #import "KeepLayout.h"
+#import "AFNetworking.h"
 
 @interface VideoViewController ()
 
@@ -31,23 +32,62 @@
 	// Do any additional setup after loading the view.
 	
 	
-	MPMoviePlayerController *moviePlayerController = [MPMoviePlayerController new];
+	UIButton *streamVideoButton = [UIButton new];
+	[self.view addSubview:streamVideoButton];
+	[streamVideoButton addTarget:self action:@selector(streamVideo) forControlEvents:UIControlEventTouchUpInside];
+	[streamVideoButton setTitle:@"Stream" forState:UIControlStateNormal];
+	[streamVideoButton setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
 	
-	NSString *link = @"http://vodfiles.dr.dk/CMS/Resources/dr.dk/NETTV/DR3/2013/11/dd50a214-8aee-4c6a-8631-70a0c671a1b1/BoesseStudier---Stereotyper--2_9a4fb08b27d24c46992e81bd967a52b4_122.mp4?ID=1629336";
-	moviePlayerController.contentURL = [NSURL URLWithString:link];
-	moviePlayerController.movieSourceType = MPMovieSourceTypeStreaming;
+	UIButton *downloadVideoButton = [UIButton new];
+	[self.view addSubview:downloadVideoButton];
+	[downloadVideoButton addTarget:self action:@selector(downloadVideo) forControlEvents:UIControlEventTouchUpInside];
+	[downloadVideoButton setTitle:@"Download" forState:UIControlStateNormal];
+	[downloadVideoButton setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
 	
-	[self.view addSubview:moviePlayerController.view];
+	NSArray *buttons = @[streamVideoButton,downloadVideoButton];
+	[buttons keepSize:CGSizeMake(200, 44)];
+	[buttons keepLeftAligned];
+	[buttons keepVerticalOffsets:KeepRequired(20.0f)];
+	((UIView *)buttons.firstObject).keepLeftInset.equal = KeepRequired(20.0f);
+	((UIView *)buttons.firstObject).keepTopInset.equal = KeepRequired(20.0f);
 	
-	moviePlayerController.view.keepInsets.equal = KeepRequired(0.0f);
-	
-	[moviePlayerController play];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+
+- (void)streamVideo
+{
+	NSString *link = @"http://vodfiles.dr.dk/CMS/Resources/dr.dk/NETTV/DR3/2013/11/dd50a214-8aee-4c6a-8631-70a0c671a1b1/BoesseStudier---Stereotyper--2_9a4fb08b27d24c46992e81bd967a52b4_122.mp4?ID=1629336";
+	NSURL *url = [NSURL URLWithString:link];
+	MPMoviePlayerViewController *moviePlayerViewController = [[MPMoviePlayerViewController alloc] initWithContentURL:url];
+	
+	[self presentMoviePlayerViewControllerAnimated:moviePlayerViewController];
+}
+
+- (void)downloadVideo
+{
+	NSString *stringURL = @"http://vodfiles.dr.dk/CMS/Resources/dr.dk/NETTV/DR3/2013/11/dd50a214-8aee-4c6a-8631-70a0c671a1b1/BoesseStudier---Stereotyper--2_9a4fb08b27d24c46992e81bd967a52b4_122.mp4?ID=1629336";
+	NSURL  *url = [NSURL URLWithString:stringURL];
+	NSData *urlData = [NSData dataWithContentsOfURL:url];
+	if ( urlData )
+	{
+		NSArray       *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+		NSString  *documentsDirectory = [paths objectAtIndex:0];
+		NSString  *filePath = [NSString stringWithFormat:@"%@/%@", documentsDirectory,@"test.mp4"];
+		[urlData writeToFile:filePath atomically:YES];
+		NSLog(@"Saved video to: %@",filePath);
+	}
+}
+
+- (void)getAllSeriesFromDR
+{
+	
 }
 
 @end
