a0d6735b786ae09d976b1b8cf3d3cbc7de0ce266
Tobias Due Munk <tobiasdm@gmail.com>
2013-11-14 01:13:43 +0100

    iOS: Get data from DR √. Parse JSON √.

--- iOS/Doktor TV/Doktor TV/Classes/VideoViewController.m
+++ iOS/Doktor TV/Doktor TV/Classes/VideoViewController.m
@@ -44,7 +44,13 @@
 	[downloadVideoButton setTitle:@"Download" forState:UIControlStateNormal];
 	[downloadVideoButton setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
 	
-	NSArray *buttons = @[streamVideoButton,downloadVideoButton];
+	UIButton *getAllSeriesFromDRButton = [UIButton new];
+	[self.view addSubview:getAllSeriesFromDRButton];
+	[getAllSeriesFromDRButton addTarget:self action:@selector(getAllSeriesFromDR) forControlEvents:UIControlEventTouchUpInside];
+	[getAllSeriesFromDRButton setTitle:@"Get all series" forState:UIControlStateNormal];
+	[getAllSeriesFromDRButton setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
+	
+	NSArray *buttons = @[streamVideoButton,downloadVideoButton,getAllSeriesFromDRButton];
 	[buttons keepSize:CGSizeMake(200, 44)];
 	[buttons keepLeftAligned];
 	[buttons keepVerticalOffsets:KeepRequired(20.0f)];
@@ -87,7 +93,50 @@
 
 - (void)getAllSeriesFromDR
 {
+	// SYNCHRON
 	
+	NSString *stringURL = @"http://www.dr.dk/mu/programcard?Title=$eq(%22Borgen%20afsnit%2012%22)";
+	NSURL  *url = [NSURL URLWithString:stringURL];
+	NSData *urlData = [NSData dataWithContentsOfURL:url];
+	NSString *urlString = [NSString stringWithContentsOfURL:url encoding:(NSUTF8StringEncoding) error:nil];
+	if ( urlData )
+	{
+		NSError* error;
+		NSDictionary* json = [NSJSONSerialization JSONObjectWithData:urlData options:kNilOptions error:&error];
+		NSLog(@"%@",json);
+		
+		NSLog(@"%@",urlString);
+	}
+	
+	return;
+	
+	
+	// AFNETWORKING
+	
+	
+	NSString *query = @"/programcard?Title=$eq(%22Borgen%20afsnit%2012%22)";
+//	query = @"/programcard?Title=$eq(\"Borgen afsnit 12\")";
+	
+	NSURL *URL = [NSURL URLWithString:@"http://www.dr.dk/mu"];
+	AFHTTPSessionManager *manager = [[AFHTTPSessionManager alloc] initWithBaseURL:URL];
+	[manager GET:query parameters:nil success:^(NSURLSessionDataTask *task, id responseObject) {
+//		[resources addObjectsFromArray:responseObject[@"resources"]];
+		
+		NSLog(@"%@",responseObject);
+//		[manager SUBSCRIBE:query usingBlock:^(NSArray *operations, NSError *error) {
+//			for (AFJSONPatchOperation *operation in operations) {
+//				switch (operation.type) {
+//					case AFJSONAddOperationType:
+//						[resources addObject:operation.value];
+//						break;
+//					default:
+//						break;
+//				}
+//			}
+//		} error:nil];
+	} failure:^(NSURLSessionDataTask *task, NSError *error) {
+		NSLog(@"%@",error);
+	}];
 }
 
 @end
